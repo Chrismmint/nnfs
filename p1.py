376765a10c04d82cc2eef58dcb5d5201e4f1ef8c
ChrisMMint <33577525+Chrismmint@users.noreply.github.com>
2020-04-29 18:40:14 +0100

    Modelled 3 neuron layer

--- p1.py
+++ p1.py
@@ -2,8 +2,8 @@
 # sum of the input from each of the neurons multiplied by that input's
 # weight, plus whatever bias the neuron has.
 
-inputs = [1,2,3]
-weights = [0.2,0.8,-0.5]
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
 bias = 2
 
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
