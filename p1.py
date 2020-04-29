41029a0ec55cdf249d334c9841e6a02409bccde3
ChrisMMint <33577525+Chrismmint@users.noreply.github.com>
2020-04-29 13:15:31 +0100

    Fixed output calc

--- p1.py
+++ p1.py
@@ -1,11 +1,11 @@
 # a basic neuron with 3 neurons connected to it - the output is the
-# sum of the input from each of the neurons multiplied by that inputs
+# sum of the input from each of the neurons multiplied by that input's
 # weight, plus whatever bias the neuron has.
 
-inputs = [1.2, 5.1, 2.1]
-weights = [2.1, 3.1, 8.7]
-bias = 3
+inputs = [1,2,3]
+weights = [0.2,0.8,-0.5]
+bias = 2
 
-output = inputs[0]*weights[0] + inputs[0]*weights[0] + inputs[0]*weights[0] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 
 print(output)
